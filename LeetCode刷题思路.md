bb98a344b2cff32d4570c43fe75323db90e14d2c
PeterHo <peter45402@163.com>
2020-08-18 22:58:51 +0800

    新增动态规划系列

--- LeetCode刷题思路.md
+++ LeetCode刷题思路.md
@@ -3458,7 +3458,24 @@
 2. 按平方数来分割整数--279--Medium
 
    ```java
-   
+   class Solution {
+       // dp[i]：表示完全平方数和为i的 最小个数
+       // 初始状态dp[i]均取最大值i，即 1+1+...+1，i个1; dp[0] = 0
+       // dp[i] = min(dp[i], dp[i-j*j]+1)，其中, j是平方数, j=1~k,其中k*k要保证 <= i
+       // 意思就是：完全平方数和为i的 最小个数 等于 当前完全平方数和为i的 最大个数
+       //   与 （完全平方数和为 i - j * j 的 最小个数 + 完全平方数和为 j * j的 最小个数）
+       //   可以看到 dp[j*j] 是等于1的
+       public int numSquares(int n) {
+           int[] dp = new int[n + 1]; // 默认初始化值都为0
+           for (int i = 1; i <= n; i++) {
+               dp[i] = i; // 最坏的情况就是每次+1
+               for (int j = 1; i - j * j >= 0; j++) {
+                   dp[i] = Math.min(dp[i], dp[i - j * j] + 1); // 动态转移方程
+               }
+           }
+           return dp[n];
+       }
+   }
    ```
 
    
--- src/FuntionTest.java
+++ src/FuntionTest.java
@@ -117,24 +117,26 @@ public class FuntionTest {
 //            }
 //            System.out.println("");
 //        }
-        Test109 test109 = new Test109();
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-        ListNode node6 = new ListNode(6);
-        ListNode node7 = new ListNode(7);
-        ListNode node8 = new ListNode(8);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node7;
-        node7.next = node8;
-        node8.next = null;
-        test109.sortedListToBST(node1);
+//        Test109 test109 = new Test109();
+//        ListNode node1 = new ListNode(1);
+//        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(3);
+//        ListNode node4 = new ListNode(4);
+//        ListNode node5 = new ListNode(5);
+//        ListNode node6 = new ListNode(6);
+//        ListNode node7 = new ListNode(7);
+//        ListNode node8 = new ListNode(8);
+//        node1.next = node2;
+//        node2.next = node3;
+//        node3.next = node4;
+//        node4.next = node5;
+//        node5.next = node6;
+//        node6.next = node7;
+//        node7.next = node8;
+//        node8.next = null;
+//        test109.sortedListToBST(node1);
+        Test279 test279 = new Test279();
+        test279.numSquares(12);
     }
 
 }
