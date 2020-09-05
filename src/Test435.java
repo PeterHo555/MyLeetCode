c575bbed92f7a5044cfca88001e05de9f987916a
PeterHo <peter45402@163.com>
2020-09-05 20:35:03 +0800

    添加贪心算法系列

--- src/Test435.java
+++ src/Test435.java
@@ -1,6 +1,20 @@
+import java.util.Arrays;
+
 public class Test435 {
     public int eraseOverlapIntervals(int[][] intervals) {
-
-        return 1;
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int cnt = 1;
+        int end = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            cnt++;
+        }
+        return intervals.length - cnt;
     }
 }
