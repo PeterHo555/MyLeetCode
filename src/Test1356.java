f6c10e628aa9b9992f33d3a566a89ded9324efe7
PeterHo <peter45402@163.com>
2020-11-09 21:46:29 +0800

    日推

---
+++ src/Test1356.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test1356 {
+    public int[] sortByBits(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int x : arr) {
+            list.add(x);
+        }
+        int[] bit = new int[10001];
+        for (int i = 1; i <= 10000; ++i) {
+            bit[i] = bit[i >> 1] + (i & 1);
+        }
+        Collections.sort(list, new Comparator<Integer>() {
+            public int compare(Integer x, Integer y) {
+                if (bit[x] != bit[y]) {
+                    return bit[x] - bit[y];
+                } else {
+                    return x - y;
+                }
+            }
+        });
+        for (int i = 0; i < arr.length; ++i) {
+            arr[i] = list.get(i);
+        }
+        return arr;
+    }
+}
---
+++ src/Test973.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Test973 {
+    public int[][] kClosest(int[][] points, int K) {
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] point1, int[] point2) {
+                return (point1[0] * point1[0] + point1[1] * point1[1]) - (point2[0] * point2[0] + point2[1] * point2[1]);
+            }
+        });
+        return Arrays.copyOfRange(points, 0, K);
+    }
+}
