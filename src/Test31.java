f219a37954991efaa0eb31b842113d3995304f64
PeterHo <peter45402@163.com>
2020-11-10 19:09:23 +0800

    日推

---
+++ src/Test31.java
@@ -0,0 +1,42 @@
+public class Test31 {
+    // 题干的意思是：找出这个数组排序出的所有数中，刚好比当前数大的那个数（人话）
+    // 比如当前 nums = [1,2,3]。这个数是123，找出1，2，3这3个数字排序可能的所有数，排序后，比123大的那个数 也就是132
+    // 如果当前 nums = [3,2,1]。这就是1，2，3所有排序中最大的那个数，那么就返回1，2，3排序后所有数中最小的那个，也就是1，2，3 -> [1,2,3]
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) return;
+        int firstIndex = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                firstIndex = i;
+                break;
+            }
+        }
+        if (firstIndex == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+        int secondIndex = -1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > nums[firstIndex]) {
+                secondIndex = i;
+                break;
+            }
+        }
+        swap(nums, firstIndex, secondIndex);
+        reverse(nums, firstIndex + 1, nums.length - 1);
+        return;
+
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i++, j--);
+        }
+    }
+
+    private void swap(int[] nums, int i, int i1) {
+        int tmp = nums[i];
+        nums[i] = nums[i1];
+        nums[i1] = tmp;
+    }
+}
