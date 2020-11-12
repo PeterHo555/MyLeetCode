88498a30362ee44dd52d511389a4d703afc78418
PeterHo <peter45402@163.com>
2020-11-12 21:30:13 +0800

    日推

--- src/Test922.java
+++ src/Test922.java
@@ -1,6 +1,17 @@
 public class Test922 {
     public int[] sortArrayByParityII(int[] A) {
-
-        return A;
+        int[] result = new int[A.length];
+        int odd = 1;
+        int even = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] % 2 == 0) {
+                result[even] = A[i];
+                even += 2; //偶数下标移动
+            } else {
+                result[odd] = A[i];
+                odd += 2; //奇数下标移动
+            }
+        }
+        return result;
     }
 }
